9c230e6f432c60bafe866332c94651108e35f8d2
shinypark88 <shinypark06@gmail.com>
2025-06-26 12:32:56 +0900

    Add files via upload

---
+++ 0625-2.py
@@ -0,0 +1,11 @@
+Python 3.13.5 (v3.13.5:6cb20a219a8, Jun 11 2025, 12:23:45) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> 
+============= RESTART: /Users/seohwanpark/Desktop/python/0625-1.py =============
+IDLE에서 파이썬 코드를
+작성해서 출력해 보는
+예제입니다
+>>> 10+10
+20
+>>> "Hello" * 3
+'HelloHelloHello'
