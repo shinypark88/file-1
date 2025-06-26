943255fa27805294d7930a48a9bac86b5d328515
shinypark88 <shinypark06@gmail.com>
2025-06-26 12:32:26 +0900

    Add files via upload

---
+++ 0626-2.py
@@ -0,0 +1,91 @@
+Python 3.13.5 (v3.13.5:6cb20a219a8, Jun 11 2025, 12:23:45) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+print("Python Programming")
+Python Programming
+10+20
+30
+273
+273
+10+20+30*10
+330
+"Python Programming"
+'Python Programming'
++
+SyntaxError: invalid syntax
+-
+SyntaxError: invalid syntax
+import keyword
+print(keyword.kwlist)
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+print("Hello! Python Programming...")
+Hello! Python Programming...
+1+1
+2
+10-10
+0
+print("Hello Python Programming...!")
+Hello Python Programming...!
+print(52)
+52
+print(273)
+273
+print(52, 273, "Hello")
+52 273 Hello
+print("안녕하세요", "저의", "이름은", "윤민성입니다!")
+안녕하세요 저의 이름은 윤민성입니다!
+print()
+
+print("#하나만 출력합니다.")
+#하나만 출력합니다.
+print("# 하나만 출력합니다.")
+# 하나만 출력합니다.
+print("Hello Python Programming...!")
+Hello Python Programming...!
+print()
+
+print("# 여러 개를 출력합니다.")
+# 여러 개를 출력합니다.
+print(10, 20, 30, 40, 50)
+10 20 30 40 50
+print("안녕하세요", "저의", "이름은", "윤민성입니다!")
+안녕하세요 저의 이름은 윤민성입니다!
+print()
+
+print("# 아무것도 출력하지 않습니다.")
+# 아무것도 출력하지 않습니다.
+print("--- 확인 전용선 ---")
+--- 확인 전용선 ---
+print()
+
+print()
+
+print("--- 확인 전용선 ---")
+--- 확인 전용선 ---
+
+============= RESTART: /Users/seohwanpark/Desktop/python/0626-1.py =============
+# 하나만 출력합니다.
+Hello Python Programming...!
+
+# 여러 개를 출력합니다.
+10 20 30 40 50
+안녕하세요 저의 이름은 윤민성입니다!
+
+# 아무것도 입력하지 않으면 단순하게 줄바꿈합니다.
+--- 확인 전용선 ---
+
+
+--- 확인 전용선 ---
+>>> 
+============= RESTART: /Users/seohwanpark/Desktop/python/0626-1.py =============
+# 하나만 출력합니다.
+Hello Python Programming...!
+
+# 여러 개를 출력합니다.
+10 20 30 40 50
+안녕하세요 저의 이름은 윤민성입니다!
+
+# 아무것도 출력하지 않습니다.
+--- 확인 전용선 ---
+
+
+--- 확인 전용선 ---
