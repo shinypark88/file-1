7336976c350945ccf2bddbd06ee2528321ebf2fb
shinypark88 <shinypark06@gmail.com>
2025-07-17 19:54:43 +0900

    Add files via upload

---
+++ 0717-수업1.py
@@ -0,0 +1,43 @@
+##for x in range(1,6):
+##    for t in range(1,x+1):
+##            print(chr(x-1+ord("A")), end = " ")
+##    print()
+
+##count = 0
+##for x in range(6):
+##    count+=x
+##    for y in range(x,x+count):
+##        print(chr(y-1+ord("A")), end = " ")
+##    print()
+##    count = 0
+    
+##string = "abcd"
+
+##for y in range(len(string)):
+##    for x in range(y, len(string)): 
+##        print(string[y:x+1])
+
+##for i in range(1,10):
+##    print(2,"*",i,"=",2*1)
+##
+##for i in range(1,10):
+##    print("2*%d=%d"%(i,2*i))
+
+##for i in range(1,10):
+##    print("{0}*{1}={2}".format(2,i,2*i))
+##
+##for i in range(1,10):
+##    print("{0}*{1}={2}".format(3,i,3*i))
+##
+##for i in range(1,10):
+##    print("{0}*{1}={2}".format(2,i,2*i))
+##
+##for i in range(1,10):
+##    print("{0}*{1}={2}".format(4,i,4*i))
+##
+##for i in range(1,10):
+##    print("{0}*{1}={2}".format(5,i,5*i))
+
+
+
+
