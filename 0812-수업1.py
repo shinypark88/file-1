7b126865a9687dac79035a71be85894c4fa28ac7
shinypark88 <shinypark06@gmail.com>
2025-08-12 16:34:17 +0900

    Add files via upload

---
+++ 0812-수업1.py
@@ -0,0 +1,12 @@
+##
+##num = int(input())
+##for n in range(0,num):
+##    allign=input()
+##    dict_n = {}
+##    for i in range(97,122+1):
+##        if chr(i) in allign:
+##            dict_n[chr(i)] = " "
+##            for char in dict_n.keys():
+##                dict_n[char]=allign.index(char)
+##                print(dict_n)
+##    print(dict_n)
