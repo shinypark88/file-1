b3ed0f745f0c9a75be4e9b4019580662acc20098
shinypark88 <shinypark06@gmail.com>
2025-08-18 16:50:45 +0900

    Add files via upload

---
+++ 0818-수업1.py
@@ -0,0 +1,109 @@
+##def fibonacci (n):
+##    if n in dictionary:
+##        return dictionary[n]
+##    else:
+##        output = fibonacci(n-1)+fibonacci(n-2)
+##        dictionary[n] = output
+##        return output
+##
+##def fibonacci(n):
+##    if n in dictionary:
+##        return dictionary[n]
+##    output = fibonacci(n-1)+fibonacci(n-2)
+##    dictionary[n] = output
+##    return output
+##
+
+##def flatten(data):
+##    output= []
+##
+##    return output
+##def flatten(data):
+##    output= []
+##    for item in data:
+##
+##    return output
+##def flatten(data):
+##    output = []
+##    for item in data:
+##        if type(item) == list:
+##            output+=item
+##        else:
+##            output.append(item)
+##    return output
+##
+##example = [[1,2,3],[4,[5,6]],7,[8,9]]
+##print("원본:",example)
+##print("변환:",flatten(example))
+##
+##
+##def flatten(data):
+##    output = []
+##    for item in data:
+##        if type(item) == list:
+##            output+=flatten(item)
+##        else:
+##            output.append(item)
+##    return output
+##
+##example = [[1,2,3],[4,[5,6]],7,[8,9]]
+##print("원본: ",example)
+##print("변환: ",flatten(example))
+
+
+##number_input_a = input("숫자 입력> ")
+##radius = float(number_input_a)
+##print(2*3.14*radius)
+##print(3.14*radius*radius)
+##
+###숫자 입력을 받습니다.
+##number_input_a = input("숫자 입력> ")
+###원의 둘레와 넓이를 출력합니다.
+##print(2*3.14*radius)
+##print(3.14*radius*radius)
+
+##def number_input():
+##    output = input("숫자 입력>")
+##    return float(output)
+##def get_circumference(radius):
+##    return 2*3.14*radius
+##def get_circle_area(radius):
+##    return 3.14*radius*radius
+##
+##radius = number_input()
+##print(get_circumference(radius))
+##print(get_circle_area(radius))
+
+##def get_circumference(radius):
+##    return 2* 3.14 * radius
+##def get_circle_area(radius):
+##    return 3.14 * radius * radius
+##
+##PI=3.14
+##
+##def get_circumference(radius):
+##    return 2 * PI * radius
+##def get_circle_area(radius):
+##    return PI * radius * radius
+##
+##print("<p>{}</p>".format("안녕하세요."))
+##print("<p>{}</p>".format("간단한 HTML 태그를 만드는 예입니다."))
+##def p(content):
+##    return "<p class='content-line'>{}</p>".format(content)
+##
+##print(p("안녕하세요."))
+##print(p("간단한 HTML 태그를 만드는 예입니다."))
+
+##앉힐수있는최소사람수 = 2
+##앉힐수있는최대사람수 = 10
+##전체사람의수 = 100
+##memo = {}
+
+def C[num,select]:
+    start=1
+    num
+
+    
+
+
+
